4f4ab8a5afedd4c49d6f76db5abb24fb654dbd3a
Brennan Seymour <brenno263@gmail.com>
2022-09-24 19:31:13 -0500

    got cheese spawning working

--- Assets/Scripts/Cheese.cs
+++ Assets/Scripts/Cheese.cs
@@ -1,29 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 public class Cheese : MonoBehaviour
 {
 
     public float health;
     public float speed;
+    public float animationVelocity;
+    public float animationHeight;
+    public float animationRotation;
+    public Action onDeath;
+    public GameObject animatedCheese;
+    public Rigidbody2D rigid2D;
+    public SpriteRenderer spriteRenderer;
 
     [Header("Set Dynamically")]
     public GameObject mouse;
-    public Rigidbody2D rigid;
     
     // Start is called before the first frame update
     void Start()
     {
-        rigid = GetComponent<Rigidbody2D>();
         mouse = GameObject.Find("mouse");
-        Vector2 dir = (transform.position - mouse.transform.position).normalized;
-        rigid.velocity = dir * speed;
+        Vector2 dir = (mouse.transform.position - transform.position).normalized;
+        rigid2D.velocity = dir * speed;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D col)
     {
+        if (col.tag == "Scream")
+        {
+            StartCoroutine(deathAnimation());
+        }
+    }
+
+    private IEnumerator deathAnimation()
+    {
+        GameObject go = Instantiate(animatedCheese, transform.position, transform.rotation);
+        Rigidbody rigid = go.GetComponent<Rigidbody>();
+        SpriteRenderer sr = go.GetComponent<SpriteRenderer>();
+
+        rigid.velocity = (Vector3)(-rigid2D.velocity * animationVelocity) + Vector3.forward * animationHeight;
+        rigid.angularVelocity = Random.insideUnitCircle.normalized * animationRotation;
+        sr.sprite = spriteRenderer.sprite;
+
+        rigid2D.simulated = false;
+        spriteRenderer.enabled = false;
         
+        yield return new WaitForSeconds(3);
+        Destroy(go);
+        onDeath.Invoke();
+        Destroy(gameObject);
     }
 }
